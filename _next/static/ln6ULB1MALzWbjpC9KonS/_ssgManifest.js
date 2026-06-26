344acbbbaaa25af477d9d4aebb45db173b1352d0
Magnetar503 <295396374+Magnetar503@users.noreply.github.com>
2026-06-26 18:31:46 +0300

    init

---
+++ _next/static/ln6ULB1MALzWbjpC9KonS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]\u002Fhome","\u002F[lang]\u002Fproject\u002F[project_id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-import 'react';
-
-declare module 'react' {
-    interface CSSProperties {
-        '--fluid-max'?: string | number
-        '--fluid-min'?: string | number
-    }
-}
